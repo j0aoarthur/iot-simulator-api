3fe7f5e532a9133c1d3a569eef8e02a6246a9949
joaoarthur <arthur1081@hotmail.com>
2025-11-25 22:15:26 -0300

    initialize IoT Simulator project with Maven setup and basic application structure

---
+++ src/test/java/com/j0aoarthur/iotsimulator/IotSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j0aoarthur.iotsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
